a14f487108e8a381cab6f2ae6daabd6b6e1c52de
tesseractx0FFFF <itkugit321@gmail.com>
2023-04-19 21:17:20 +0300

    Created a book object constructor. The constructor's prototype property includes a 'hasBeenread' function to set a book's status. made book DOM elements flexbox.

---
+++ js/libraryApp.js
@@ -0,0 +1,20 @@
+// Books are stored here.
+let myLibrary = [];
+
+// Just a book object constructor.
+function Book(author, title, numOfPages, read){
+    this.author = author;
+    this.title = title;
+    this.numOfPages = numOfPages;
+    this.read = Boolean(read);
+}
+
+// Every book object will inherit a 'has been read' function.
+Book.prototype.read = function(){
+    if(this.read){
+        this.read = false;
+    }
+    else{
+        this.read = true;
+    }
+}
